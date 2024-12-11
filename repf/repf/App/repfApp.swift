e14c5a654e9f2101efddc9813c791f461def2a79
김우빈 <oobin765@gmail.com>
2024-12-12 02:10:54 +0900

    firebase 추가

--- repf/repf/App/repfApp.swift
+++ repf/repf/App/repfApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct repfApp: App {
+    init() {
+           FirebaseApp.configure()
+       }
     var body: some Scene {
         WindowGroup {
             SplashView()
