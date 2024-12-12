d521a23ae671e4a040718723662d052be8ea2785
김우빈 <oobin765@gmail.com>
2024-12-12 19:28:46 +0900

    스플래시 단순 이미지에서 코드로 변경

--- repf/repf/View/Screens/SplashView.swift
+++ repf/repf/View/Screens/SplashView.swift
@@ -1,44 +1,42 @@
 import SwiftUI
 
 struct SplashView: View {
-    @State private var isButtonEnabled = true // 버튼 활성화 상태
-    
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                // 배경 이미지
-                Image("SplashBackgraound")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .ignoresSafeArea()
-                
-                VStack {
-                    Spacer() // 상단 여백
-                    
-                    VStack(spacing: 16) { // 버튼 그룹
-                        // 첫 번째 버튼
-                        PrimaryButton(
-                            title: "시작하기",
-                            isEnabled: isButtonEnabled,
-                            action: {
-                                print("시작하기 버튼 클릭됨!")
-                            }
-                        )
-                        .padding(.horizontal, 16)
-                        
-                        // 두 번째 버튼
-                        PrimaryButton(
-                            title: "구글로 시작하기",
-                            isEnabled: isButtonEnabled,
-                            action: {
-                                print("구글로 시작하기 버튼 클릭됨!")
-                            },
-                            enabledColor: Color.white
-                        )
-                        .padding(.horizontal, 16)
-                    }
-                    .padding(.bottom, geometry.safeAreaInsets.bottom + 80) // 하단 여백
+        ZStack {
+            Color.appBlack
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Image("TailTop")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 252.29, height: 136.96)
+                        .offset(x: 45, y: 95)
                 }
+                .padding(.trailing, 10)
+                .padding(.top, 10)
+
+                Spacer()
+
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 193, height: 115)
+                
+                Spacer()
+
+                HStack {
+                    Image("TailBottom")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 197, height: 158)
+                        .offset(x: -20, y: -100)
+                    Spacer()
+                }
+                .padding(.leading, 10)
+                .padding(.bottom, 10)
             }
         }
     }
@@ -46,12 +44,6 @@ struct SplashView: View {
 
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            SplashView()
-                .previewDevice("iPhone 14")
-            
-            SplashView()
-                .previewDevice("iPad Pro (12.9-inch) (6th generation)") // iPad 미리보기
-        }
+        SplashView()
     }
 }
